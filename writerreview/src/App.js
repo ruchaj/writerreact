aae3fdf81ecebdf461188b6d6ec8d73548be8e09
ruchaj <ruchajrocks@gmail.com>
2022-02-19 14:15:35 -0800

    push

--- writerreview/src/App.js
+++ writerreview/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Component } from 'react';
+import './Login.js'
 function App() {
   return (
-    <div className="app">
-
-    </div>
+  <div className="app">
+    <Router>
+        <Route exact path="/" component={Login} />
+    </Router>
+  </div>
   );
 }
 export default App;
