68634937e2c0fdd63c41c1a2656be0cc5e230b8e
ruchaj <ruchajrocks@gmail.com>
2022-02-19 14:22:16 -0800

    fuxhjfad

--- writerreview/src/firebase.js
+++ writerreview/src/firebase.js
@@ -1,23 +1,23 @@
 import { initializeApp } from "firebase/app";
-​​import {
-​​  GoogleAuthProvider,
-​​  getAuth,
-​​  signInWithPopup,
-​​  signInWithEmailAndPassword,
-​​  createUserWithEmailAndPassword,
-​​  sendPasswordResetEmail,
-​​  signOut,
-​​} from "firebase/auth";
-​​import {
-​​  getFirestore,
-​​  query,
-​​  getDocs,
-​​  collection,
-​​  where,
-​​  addDoc,
-​​} from "firebase/firestore";
+
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+ signOut,
+} from "firebase/auth";
+import {
+ getFirestore,
+ query,
+ getDocs,
+ collection,
+ where,
+ addDoc,
+} from "firebase/firestore";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
