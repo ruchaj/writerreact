f9a661ec5c6f5546b928aa45726780b8d4d39c93
ruchaj <ruchajrocks@gmail.com>
2022-02-19 15:37:13 -0800

    Tried doing Login

--- writerreview/src/App.js
+++ writerreview/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react';
 import Login from './Login';
 import './Login.css'
+
 function App() {
   return (
   <div className="app">
     <Router>
         <Routes>
-        <Route exact path="/" element={Login} />
+         <Route exact path="/Login">  <Login /> </Route>
+
       </Routes>
 
     </Router>
--- writerreview/src/firebase.js
+++ writerreview/src/firebase.js
@@ -1,34 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAjMR1Le6O4oFquHHrHraNybUFhlJDeRKE",
-  authDomain: "writerreview.firebaseapp.com",
-  databaseURL: "https://writerreview-default-rtdb.firebaseio.com",
-  projectId: "writerreview",
-  storageBucket: "writerreview.appspot.com",
-  messagingSenderId: "877240818438",
-  appId: "1:877240818438:web:0c20c53a8bbd3ffd5c9c87",
-  measurementId: "G-70SCYG5JP5"
-};
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const googleProvider = new GoogleAuthProvider();
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-/*import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import {
   GoogleAuthProvider,
-  getAuth,
   signInWithPopup,
-  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
  signOut,
@@ -64,7 +46,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const googleProvider = new GoogleAuthProvider();
 const auth = getAuth(app);
-const db = getFirestore(app);*/
+const db = getFirestore(app);
 
 const signInWithGoogle = async () => {
   try {
