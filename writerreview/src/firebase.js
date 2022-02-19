cd7dec8be65df20c81c2fcb959a35460c031baf0
ruchaj <ruchajrocks@gmail.com>
2022-02-19 14:06:59 -0800

    Create firebase.js

---
+++ writerreview/src/firebase.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "firebase/app";
+​​import {
+​​  GoogleAuthProvider,
+​​  getAuth,
+​​  signInWithPopup,
+​​  signInWithEmailAndPassword,
+​​  createUserWithEmailAndPassword,
+​​  sendPasswordResetEmail,
+​​  signOut,
+​​} from "firebase/auth";
+​​import {
+​​  getFirestore,
+​​  query,
+​​  getDocs,
+​​  collection,
+​​  where,
+​​  addDoc,
+​​} from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjMR1Le6O4oFquHHrHraNybUFhlJDeRKE",
+  authDomain: "writerreview.firebaseapp.com",
+  databaseURL: "https://writerreview-default-rtdb.firebaseio.com",
+  projectId: "writerreview",
+  storageBucket: "writerreview.appspot.com",
+  messagingSenderId: "877240818438",
+  appId: "1:877240818438:web:0c20c53a8bbd3ffd5c9c87",
+  measurementId: "G-70SCYG5JP5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+  const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = res.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+      });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+  const sendPasswordReset = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset link sent!");
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+  const logout = () => {
+    signOut(auth);
+  };
+        
